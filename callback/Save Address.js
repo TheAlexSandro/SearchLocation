f39537d42733eac89069cdec0201a05fdf325f41
Jevrin Garmindo <94431344+Jevrin@users.noreply.github.com>
2021-11-16 19:13:58 +0700

    save address

---
+++ callback/Save Address.js
@@ -0,0 +1,8 @@
+if (/svAddres$/i.exec(cb.data)){
+    var setAdd = user.getValue('esan_'+msg.chat.id)
+    user.setValue('pesanWelcome_'+msg.chat.id, setAdd)
+    var pesan = "This address have been save ✅"
+    pesan += "\nTo check it. Send /myaddress"
+    tg.answerCallbackQuery(cb.id, pesan, true)
+   return;
+  }
