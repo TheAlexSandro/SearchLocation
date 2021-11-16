e7b3b20754e76247bd1ecc0aff105ac1d4e1ffe0
Jevrin Garmindo <94431344+Jevrin@users.noreply.github.com>
2021-11-16 19:05:56 +0700

    Delete Func Callback.js

--- Func Callback.js
+++
@@ -1,9 +0,0 @@
-// callback detection
-  if (update.callback_query) {  
-    return prosesCallback(update.callback_query)
-  }
-
-// function callback
-function prosesCallback(cb) {
-  var msg = cb.message;
-}
