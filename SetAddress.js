07df1c05bd0ce02f331450d0141fe5e5ca00bbd6
Jevrin <94431344+Jevrin@users.noreply.github.com>
2021-11-16 13:55:24 +0700

    Set Address.js

---
+++ SetAddress.js
@@ -0,0 +1,20 @@
+if (cocok = /^([\/]setaddress )/i.exec(msg.text)){
+  var pesanWelcome = msg.text.replace(cocok[1],'');
+
+  try {
+
+    tg.sendMsg(msg, '✅ <b>Done</b>\nYour address have been save.\nTo check it, send /myaddress', 'HTML', false);
+    
+    return user.setValue('pesanWelcome_'+msg.chat.id, pesanWelcome);
+
+  } catch (e) {
+    var pesanError = e.message;
+    
+    if (error = /({(?:.*)})/gmi.exec(pesanError) )
+    pesanError = error[1];        
+
+    tg.sendMsg(msg, '⛔️ ERROR: '+pesanError, false, false, msg.message_id);
+  }
+
+  return;
+}
