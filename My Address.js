3ca9274a5f6c83f11d4410033c043c1ef4687369
Jevrin <94431344+Jevrin@users.noreply.github.com>
2021-11-16 14:04:10 +0700

    To view saved addresses

---
+++ My Address.js
@@ -0,0 +1,21 @@
+var pola = /^[!\/]myaddress$/i;
+if (pola.exec(msg.text) ){
+
+  var pesanWelcome = user.getValue('pesanWelcome_'+msg.chat.id);
+  
+  // jika kosong variablenya
+  if (!pesanWelcome) return tg.sendMsg(msg, "⚠️ You haven't saved the address at all.\nTo save use the following format:\n/setaddress your address", false, false, msg.message_id);
+  
+  // If it doesn't exist/hasn't set the address
+  var psn = "Your address is: <code>"+pesanWelcome+"</code>"
+  psn += "\n\n<b>To get location use:</b>"
+  psn += "\n<code>/loc "+pesanWelcome+"</code>"
+  var keyb = []
+
+  keyb[0] = [
+    tg.button.text('✏️ Edit Address', 'edit_address'),
+    tg.button.text('❌ Close', 'me_del')
+  ]
+
+  return tg.sendMsgKeyboardInline(msg, psn, keyb, 'html')
+}
