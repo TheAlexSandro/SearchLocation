57a3e9190caf2297f0e13fec0e2ca2ff44bc56f7
Jevrin Garmindo <94431344+Jevrin@users.noreply.github.com>
2021-11-16 16:27:59 +0700

    To view address

--- See My Address.js
+++ See My Address.js
@@ -3,9 +3,9 @@ if (pola.exec(msg.text) ){
 
   var pesanWelcome = user.getValue('pesanWelcome_'+msg.chat.id);
   
+  // If it doesn't exist/hasn't set the address
   if (!pesanWelcome) return tg.sendMsg(msg, "⚠️ You haven't saved the address at all.\nTo save use the following format:\n/setaddress your address", false, false, msg.message_id);
   
-  // If it doesn't exist/hasn't set the address
   var psn = "Your address is: <code>"+pesanWelcome+"</code>"
   psn += "\n\n<b>To get location use:</b>"
   psn += "\n<code>/loc "+pesanWelcome+"</code>"
