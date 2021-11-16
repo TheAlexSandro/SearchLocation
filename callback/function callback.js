45a164b311922af153cf382f0eb3b62a509539b9
Jevrin Garmindo <94431344+Jevrin@users.noreply.github.com>
2021-11-16 19:07:24 +0700

    Function for callback

--- callback/function callback.js
+++ callback/function callback.js
@@ -1,3 +1,9 @@
+// callback detection
+  if (update.callback_query) {   
+    return prosesCallback(update.callback_query)
+  }
+
+// function for callback
 function prosesCallback(cb) {
   var msg = cb.message;
 }
