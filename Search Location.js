1ecdac955acc28e444e4f4d4d03a968be03962a4
Jevrin Garmindo <94431344+Jevrin@users.noreply.github.com>
2021-11-16 18:02:47 +0700

    to Search Location

--- Search Location.js
+++ Search Location.js
@@ -1,22 +1,23 @@
-    var pola = /^[\/](?:lok|loc) (-?\d+(?:\.\d+)?),\s*(-?\d+(?:\.\d+)?)/i;
-     if (cocok = pola.exec(msg.text)) {
-      var cck = msg.text.replace(cocok[1], cocok[2]);
-
+var pola = /^[\/!](lok|loc) ([\w\s,\.]{3,})/i;
+    if (cocok = pola.exec(msg.text)) {
+      var cck = msg.text.replace(cocok[1], '');
+      
       var geocoder = Maps.newGeocoder().setLanguage('en');
-      var response = geocoder.reverseGeocode(cocok[1], cocok[2]);
+      var response = geocoder.geocode(cocok[2]);
 
       if (response.status !== 'OK') return tg.sendMsg(msg, '⚠️ <a href="tg://user?id=2085338522">Location not found</a>.', 'HTML', false, msg.message_id);
 
       var Lokasi = response.results[0];
-      
-      var title = 'Location ⬇️';
 
       var address = Lokasi.formatted_address;
       var latitude = Lokasi.geometry.location.lat;
       var longitude = Lokasi.geometry.location.lng;
+        
+      var title = 'Location ⬇️';
 
       var hasil = tg.sendMsg(msg, "⏳ Searching...", 'html')
       var newMsg = hasil.result;
+      var pHasil = "Result of: " + cck + ""
       var esan = "<code>" + address + "</code>"
       var keyb = []
 
@@ -29,13 +30,13 @@
       keyb[2] = [
         tg.button.text('📁 Save As Address', 'svAddres')
       ]
-      
+
       user.setValue('esan_'+msg.chat.id, esan)
 
       tg.util.sleep(1400)
-      tg.deleteMessage(msg.chat.id, newMsg.message_id)
+      tg.editMessageText(msg.chat.id, newMsg.message_id, newMsg.inline_message_id, pHasil, 'html', true)
       tg.sendVenue(msg.chat.id, latitude, longitude, false, false, false)
       tg.sendMsgKeyboardInline(msg, esan, keyb, 'html')
       return;
-         
+
     }
